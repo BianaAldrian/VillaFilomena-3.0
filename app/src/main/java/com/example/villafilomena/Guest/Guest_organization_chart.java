afafcbc3656bbc57af046ef8a1e0996c14eff2cf
Meng <curamengsheerymae@gmail.com>
2023-06-16 09:25:27 +0800

    meng1

---
+++ app/src/main/java/com/example/villafilomena/Guest/Guest_organization_chart.java
@@ -0,0 +1,16 @@
+package com.example.villafilomena.Guest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.villafilomena.R;
+
+public class Guest_organization_chart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_organization_chart);
+    }
+}
